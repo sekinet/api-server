b82a5be13622b702f58f53e39b6f4463a523a157
Rintaro Sekino <rintaro.sekino@ntti3.com>
2017-03-22 22:11:21 +0900

    add new mock api server

---
+++ api-server.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, make_response
+
+api = Flask(__name__)
+
+@api.route('/virtual-network/rintaro', methods=['GET'])
+def get_virtual_network():
+    result = {
+        "vmi":"UP", 
+        "version":2
+        }
+
+    return make_response(jsonify(result))
+
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=3000)
